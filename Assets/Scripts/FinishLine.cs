912290d9c3c1ec57ad632ca2fa59643b10f35fc6
Semicolon-GD <124124905+Semicolon-GD@users.noreply.github.com>
2024-05-19 14:14:09 +0300

    main assets imported

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.CompareTag("Player"))
+      {
+        EventManager.Trigger(EventList.OnFinishLineCrossed);
+      }
+   }
+
+
+
+
+   
+}
